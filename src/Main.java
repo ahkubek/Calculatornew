43629168e771bb7613eaac9186c5ef55e1284e29
ahkubek <120653318+ahkubek@users.noreply.github.com>
2024-02-05 08:39:50 +0300

    calc01.24

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,9 @@
 import java.util.*;
+import java.util.Scanner;
 public class Main {
             public static void main(String[] args)throws java.io.IOException {
-                char ch;
+      math.logik();
 
-
- System.out.println("это калькулятор \n Введи первое число");
-
-
-                    Scanner scan = new Scanner(System.in);
-                    double a = scan.nextInt();
-// System.out.println ("Вы ввели "+a);
-                    System.out.println("Введите второе число");
-                    double b = scan.nextInt();
-//System.out.println ("Вы ввели "+b);
-
-                    System.out.println ("что нужно сделать? (*) (/) (+) (-)(%)?");
-//int c = scan.nextInt();
-                    ch =(char)System.in.read();
-
-                    switch(ch){
-                        case '*': System.out.println("Ответ"+(a*b));
-                            break;
-                        case '/': System.out.println("Ответ"+(a/b));
-                            break;
-                        case '+': System.out.println("Ответ"+(a+b));
-                        break;
-                        case '-': System.out.println("Ответ"+(a-b));
-                        break;
-                        case '%': System.out.println(a/100*b);
-                    }
 
             }
         }
